68c47c81772131b1a2576f3bf757e3de86643381
Kaushal Kahar Goud <159502825+KaushalGoud@users.noreply.github.com>
2026-08-13 21:47:39 +0545

    Time: 0 ms (100%), Space: 76.3 MB (15.18%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -3,15 +3,19 @@ public:
     int maxSubArray(vector<int>& nums) {
         int currentSum = 0;
         int maxSum = INT_MIN;
+        vector<int>vp;
         if (nums.size() == 1)
             return nums[0];
 
         for (int i = 0; i < nums.size(); i++) {
             if(currentSum+nums[i]>nums[i]){
                 currentSum+=nums[i];
+                vp.push_back(nums[i]);
             }
             else{
                 currentSum=nums[i];
+                vp.clear();
+                vp.push_back(nums[i]);
 
             }
             if (maxSum < currentSum) {
